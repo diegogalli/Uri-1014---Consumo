9da0c831657a716c166c817a552c918c16f8a901
diegogalli <84423207+diegogalli@users.noreply.github.com>
2021-08-11 14:14:54 -0300

    Uri 1014 Consumo

---
+++ Consumo/Consumo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace Consumo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int x;
+            double y, combustivel;
+
+            x = int.Parse(Console.ReadLine());
+            y = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            combustivel = (double)x / y;
+
+            Console.WriteLine(combustivel.ToString("F3", CultureInfo.InvariantCulture) + " km/l");
+
+
+
+        }
+    }
+}
